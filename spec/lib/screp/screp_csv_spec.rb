1ff436872e7a1c00dd301607dde1f271c3772577
NetsydeMiro <miro@netsyde.com>
2013-08-29 17:23:05 -0400

    refactored csv spec to use tmp directory

--- spec/lib/screp/screp_csv_spec.rb
+++ spec/lib/screp/screp_csv_spec.rb
@@ -2,64 +2,73 @@ require_relative '../../spec_helper'
 
 describe Screp::Screp do
 
-  def clear_csv_files
-      Dir.glob('*.csv').each {|f| File.delete(f)}
+  def fixture_file(filename)
+    File.join('spec/fixtures', filename)
+  end
+
+  def temp_file(filename)
+    File.join('tmp', filename)
+  end
+
+  def null_input
+    fixture_file 'null_content.html'
+  end
+
+  def csv_output
+    temp_file 'test.csv'
   end
 
   describe "CSV Logging" do 
 
     before :all do 
-      Dir.mkdir('spec/fixtures/temp')
-      Dir.chdir('spec/fixtures/temp')
+      Dir.mkdir('tmp') if not Dir.exist?('tmp')
+      FileUtils.rm_rf Dir.glob('tmp/*')
     end
 
     after :each do
-      require 'pry'; binding.pry
-      FileUtils.rm_rf Dir.glob('./*')
-    end
-
-    after :all do 
-      require 'pry'; binding.pry
-      Dir.chdir('../../..')
-      FileUtils.rm_rf 'spec/fixtures/temp'
-    end
-
-    it "ouputs correct filename" do 
-      screp = Screp::Screp.new('../csv_test_null_content.html')
-      screp.init_csv(filename: 'differentfilename.csv')
-
-      screp.output_csv
-
-      File.exists?('differentfilename.csv').should be_true
+      FileUtils.rm_rf Dir.glob('tmp/*')
     end
 
     it "ouputs correct default filename" do 
-      screp = Screp::Screp.new('../csv_test_null_content.html')
+      screp = Screp::Screp.new('spec/fixtures/null_content.html')
       screp.init_csv
 
       screp.output_csv
 
-      File.exists?('___csv_test_null_content_html.csv').should be_true
+      File.exists?('spec_fixtures_null_content_html.csv').should be_true
+
+      File.delete 'spec_fixtures_null_content_html.csv'
     end
 
+    it "ouputs correct filename" do 
+      screp = Screp::Screp.new(null_input)
+      screp.init_csv(filename: temp_file('differentfilename.csv'))
+      screp.output_csv
+
+      File.exists?(temp_file 'differentfilename.csv').should be_true
+    end
+
+
     it "writes headers if specified" do 
-      screp = Screp::Screp.new('../csv_test_null_content.html')
-      screp.init_csv(headers: ['header1', 'header2', 'header3'])
+      screp = Screp::Screp.new(null_input)
+      screp.init_csv(filename: csv_output, 
+                     headers: ['header1', 'header2', 'header3'])
 
       screp.output_csv
 
-      File.readlines('___csv_test_null_content_html.csv').first.strip.should == 'header1,header2,header3'
+      File.readlines(csv_output).first.strip.should == 'header1,header2,header3'
     end
 
     it "writes data correctly" do 
-      screp = Screp::Screp.new('../csv_test_null_content.html')
+      screp = Screp::Screp.new(null_input)
+      screp.init_csv(filename: csv_output)
 
       screp.csv 'test1', 'test2'
       screp.csv 'test3', 'test4'
 
       screp.output_csv
 
-      lines = File.readlines('___csv_test_null_content_html.csv')
+      lines = File.readlines(csv_output)
       
       lines.first.strip.should == 'test1,test2'
       lines.last.strip.should == 'test3,test4'
