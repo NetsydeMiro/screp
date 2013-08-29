a77e5822135ee28151939fc194920ef91a22874f
NetsydeMiro <miro@netsyde.com>
2013-08-29 15:22:40 -0400

    testing new file related testing approach

--- spec/lib/screp/screp_csv_spec.rb
+++ spec/lib/screp/screp_csv_spec.rb
@@ -9,20 +9,23 @@ describe Screp::Screp do
   describe "CSV Logging" do 
 
     before :all do 
-      Dir.chdir('spec/fixtures')
+      Dir.mkdir('spec/fixtures/temp')
+      Dir.chdir('spec/fixtures/temp')
     end
 
-    before :each do
-      clear_files('.', '*.csv')
+    after :each do
+      require 'pry'; binding.pry
+      FileUtils.rm_rf Dir.glob('./*')
     end
 
     after :all do 
-      clear_files('.', '*.csv')
-      Dir.chdir('../..')
+      require 'pry'; binding.pry
+      Dir.chdir('../../..')
+      FileUtils.rm_rf 'spec/fixtures/temp'
     end
 
     it "ouputs correct filename" do 
-      screp = Screp::Screp.new('csv_test_null_content.html')
+      screp = Screp::Screp.new('../csv_test_null_content.html')
       screp.init_csv(filename: 'differentfilename.csv')
 
       screp.output_csv
@@ -31,32 +34,32 @@ describe Screp::Screp do
     end
 
     it "ouputs correct default filename" do 
-      screp = Screp::Screp.new('csv_test_null_content.html')
+      screp = Screp::Screp.new('../csv_test_null_content.html')
       screp.init_csv
 
       screp.output_csv
 
-      File.exists?('csv_test_null_content_html.csv').should be_true
+      File.exists?('___csv_test_null_content_html.csv').should be_true
     end
 
     it "writes headers if specified" do 
-      screp = Screp::Screp.new('csv_test_null_content.html')
+      screp = Screp::Screp.new('../csv_test_null_content.html')
       screp.init_csv(headers: ['header1', 'header2', 'header3'])
 
       screp.output_csv
 
-      File.readlines('csv_test_null_content_html.csv').first.strip.should == 'header1,header2,header3'
+      File.readlines('___csv_test_null_content_html.csv').first.strip.should == 'header1,header2,header3'
     end
 
     it "writes data correctly" do 
-      screp = Screp::Screp.new('csv_test_null_content.html')
+      screp = Screp::Screp.new('../csv_test_null_content.html')
 
       screp.csv 'test1', 'test2'
       screp.csv 'test3', 'test4'
 
       screp.output_csv
 
-      lines = File.readlines('csv_test_null_content_html.csv')
+      lines = File.readlines('___csv_test_null_content_html.csv')
       
       lines.first.strip.should == 'test1,test2'
       lines.last.strip.should == 'test3,test4'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'simplecov'
+require 'fileutils'
 
 SimpleCov.start
 
@@ -6,7 +7,3 @@ require_relative '../lib/screp'
 require_relative '../lib/screp/text_utilities'
 require_relative '../lib/screp/http_utilities'
 require_relative '../lib/screp/general_utilities'
-
-def clear_files(dir, glob)
-  Dir.glob(File.join(dir, glob)).each {|f| File.delete(f)}
-end
