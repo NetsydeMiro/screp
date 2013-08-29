349f9cab0fd6f7be17082e0769e89ed4537c1f52
NetsydeMiro <miro@netsyde.com>
2013-08-29 19:46:31 -0400

    Forgotten changes & fixed download spec

--- lib/screp/screp.rb
+++ lib/screp/screp.rb
@@ -16,7 +16,7 @@ module Screp
       @selected = [@page]
 
       @csv = []
-      @download = []
+      @downloads = []
     end
 
     attr_accessor :page
@@ -89,8 +89,13 @@ module Screp
 
     ### DOWNLOADING ###
 
+
     def download(download)
-      @download << download
+      @downloads << format_download(download)
+    end
+
+    def update_progress(current, total, filename)
+      print "\r\eDownloading file #{current} of #{total}, #{filename}".ljust(80)[0..79]
     end
 
     def init_download(options = {})
@@ -99,11 +104,6 @@ module Screp
     end
 
     def perform_download(&progress)
-      cr_clear = "\r\e"
-
-      progress ||= Proc.new do |current_dl, total_dls, dl|
-        print "#{cr_clear}Downloading file #{current_dl} of #{total_dls}, #{dl.values.last}"
-      end
 
       init_download if !@directory
 
@@ -113,34 +113,37 @@ module Screp
       successful_downloads = []
       failed_downloads = []
 
-      total_downloads = @download.length
+      total_downloads = @downloads.length
 
-      @download.each_with_index do |dl, index|
+      @downloads.each_with_index do |download, index|
 
-        progress.call index+1, total_downloads, dl
+        remote_url = download.keys.first
+        local_filename = download[remote_url]
 
-        remote_url = dl.keys.first
-        local_filename = dl[remote_url]
+        update_progress index+1, total_downloads, local_filename
+
         local_filepath = File.join(@directory, local_filename)
 
         if !@overwrite && File.exists?(local_filepath)
-          existing_downloads << dl
+          existing_downloads << download
         else
           begin
             temp_file = open(remote_url, 'rb')
             local_file = File.open(local_filepath, 'w+b') 
             local_file.write(temp_file.read)
-            successful_downloads << dl
+            local_file.close
+            successful_downloads << download
           rescue SignalException => signal
             # this is so that we can ctrl-c to end the program
             File.delete(local_filepath) if File.exists?(local_filepath)
             raise signal
           rescue Exception => ex
-            failed_downloads << dl.merge(ex: ex)
+            failed_downloads << download.merge(ex: ex)
             File.delete(local_filepath) if File.exists?(local_filepath)
           end
         end
       end
+
       create_download_report(failed_downloads, 
                              existing_downloads, 
                              successful_downloads)
--- spec/lib/screp/screp_download_spec.rb
+++ spec/lib/screp/screp_download_spec.rb
@@ -2,11 +2,8 @@ require_relative '../../spec_helper'
 
 describe Screp::Screp do
 
-  def csv_output
-    temp_file 'test.csv'
-  end
 
-  describe "Downloading" do 
+  describe "Downloading Functionality" do 
 
     before :all do 
     end
@@ -27,10 +24,21 @@ describe Screp::Screp do
 
     it "creates correct specified directory" do 
       screp = Screp::Screp.new('spec/fixtures/null_content.html')
-      screp.init_download(directory: temp_file('new_directory'))
+      screp.init_download(directory: temp_file('download_directory'))
       screp.perform_download
 
-      Dir.exists?(temp_file 'new_directory').should be_true
+      Dir.exists?(temp_file 'download_directory').should be_true
+    end
+
+    it "downloads specified file correctly" do 
+      screp = Screp::Screp.new('spec/fixtures/null_content.html')
+      screp.init_download(directory: temp_file('download_directory'))
+
+      screp.download fixture_file('download.txt') => 'new_name.txt' 
+      screp.perform_download
+
+      File.exists?(temp_file 'download_directory/new_name.txt').should be_true
+      File.readlines(temp_file 'download_directory/new_name.txt').should == File.readlines(fixture_file 'download.txt')
     end
 
   end
